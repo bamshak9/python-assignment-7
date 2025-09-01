1f8acd608f15166ef0cbf6b76d960e1331339ab8
bamshak9 <b.fwangyil@alustudent.com>
2025-09-01 20:49:48 +0100

    first commit

---
+++ attendance.py
@@ -0,0 +1,45 @@
+"""
+Attendance Register
+
+Task:
+- Track attendance of students.
+- Use a dictionary { "student_id": {"name": str, "present_days": list, "absent_days": list} }
+- Functions to mark attendance, check history, and get reports.
+- Use your head/logic to mark multiple students at once.
+- Use **kwargs for flexible reporting (e.g., only_present=True).
+
+// NOT FOR THIS ASSIGNMENT
+Future OOP Extension:
+- Student class with mark_present() and mark_absent().
+- AttendanceRegister class that manages records.
+"""
+
+import datetime
+
+attendance = {}
+
+def register_student(student_id, name):
+    attendance[student_id]=name
+    """Register a student in the system."""
+    return attendance
+
+def mark_present(student_ids):
+    """Mark multiple students as present for today."""
+    today = str(datetime.date.today())
+    # implement logic
+    pass
+
+def mark_absent(student_ids):
+    """Mark multiple students as absent for today."""
+    today = str(datetime.date.today())
+    #implement logic
+    pass
+
+def get_report(**kwargs):
+    """Generate attendance report with optional filters."""
+    report = {}
+    # implement logic
+
+    return report
+name={{"name": str, "present_days": list, "absent_days": list}}
+print(register_student(3124,name))
---
+++ library.py
@@ -0,0 +1,38 @@
+"""
+Library Management System
+
+Task:
+- Create functions to manage a library using dictionaries and lists.
+- Each book is stored in a dictionary with fields: { "id": int, "title": str, "author": str, "available": bool }
+- Users can borrow and return books.
+- Support *args for searching books by multiple fields (title, author).
+- Support **kwargs for adding optional book details like "year", "genre".
+
+
+// NOT FOR THIS ASSIGNMENT
+Future OOP Extension:
+- Books as a Book class.
+- Library as a Library class with borrow() and return() methods.
+"""
+
+library = []
+
+def add_book(**book):
+    library.append(book)
+    return "Book {title} added successfully"
+    """Add a new book into the library with flexible details.
+        return "Book {book_title} added successfully!"
+    """
+
+def search_books(search_param):
+    """Search for books by multiple keywords (title, author).
+    return books that match search description.
+    """
+
+def borrow_book(book_id):
+    """Borrow a book if available (msg: You borrowed {book_title}).
+        else-> msg: Book {book_title} not available
+    """
+print(add_book("id": 1, "title": "Genk", "author": "Jeremiah", "available": True))
+print(add_book("id": 2, "title": "kishi", "author": "John", "available": True))
+print(library)
