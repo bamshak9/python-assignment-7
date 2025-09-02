681ba342dbb1ad2aedf1870a6089925875cc7b9d
bamshak9 <b.fwangyil@alustudent.com>
2025-09-02 11:46:17 +0100

    added banking task

--- banking.py
+++ banking.py
@@ -16,21 +16,53 @@ Future OOP Extension:
 
 accounts = {}
 
-def create_account(account_number, name, **kwargs):
+def create_account(account_number, name, **details):
     """Create an account with optional features like overdraft_limit."""
-    pass
+    accounts[account_number] = {"name":name, "details": details }
+    print(accounts)
+    
+create_account(793124, "Jerry", Balance = 17000, Type = "Savings")
 
 def deposit(account_number, amount):
     """Deposit money into account.
         return "Account not found!" (if account does not exists)
         return Deposited {amount} into {accounts name}'s account. if account exists
     """
-    pass
+    if account_number in accounts:
+        accounts[account_number]["details"]["Balance"] += amount
+        print(f"The sum of {amount:,.2f} was deposited into account: {account_number} successfully!")
+    else:
+        return "Account not found!"
+    print(accounts)
+print(deposit(793124, 25000))
+
 
 def withdraw(account_number, amount):
     """Withdraw money if balance is sufficient. else: insufficient funds"""
-    pass
+    if account_number in accounts:
+        if amount <= accounts[account_number]["details"]["Balance"]:
+            accounts[account_number]["details"]["Balance"] -= amount
+        else:
+            return "Insufficicient funds!"
+    else:
+        return "Account not found!"
+    print(accounts)
+print(withdraw(793124, 9000))
+    
 
 def transfer(from_acc, to_acc, amount):
     """Transfer money between accounts. if funds is sufficient"""
-    pass
+    if from_acc in accounts and to_acc in accounts:
+        if amount <= accounts[from_acc]["details"]["Balance"]:
+            accounts[from_acc]["details"]["Balance"] -= amount
+            accounts[to_acc]["details"]["Balance"] += amount
+            print(f"The sum of {amount:,.2f} was transfered into account: {to_acc} from account: {from_acc}")
+
+        else:
+            return "Insufficicient funds!"
+    elif to_acc not in accounts:
+        return "Receiving account not found"
+    else:
+        return "Sending account not found!"
+    print(accounts)
+print(transfer(793124, 793117, 5000))
